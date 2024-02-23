3b9bfb18da1481478b2df02aa4322a66c56b9d1b
bdhamithkumara <damithkumararoxx55@gmail.com>
2024-02-23 21:54:45 +0530

    aa

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import BucketSection from "./components/BucketSection";
-import OurStorySection from "./components/ourStorySection/ourStorySection";
+
 
 export default function Home() {
   return (
